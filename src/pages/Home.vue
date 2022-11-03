0a6e5cabff4dec9eee828edabe92863b717cae31
DaveDeDave <76876642+DaveDeDave@users.noreply.github.com>
2022-11-03 19:50:47 +0100

    Added prettier plugin for tailwindcss

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,24 @@
 <script setup>
-import { ref } from "vue";
-import { useI18n } from "vue-i18n";
-import LocaleChanger from "../components/LocaleChanger.vue";
+  import { ref } from "vue";
+  import { useI18n } from "vue-i18n";
+  import LocaleChanger from "../components/LocaleChanger.vue";
 
-const { t } = useI18n();
+  const { t } = useI18n();
 
-const clicks = ref(0);
+  const clicks = ref(0);
 
-const increaseClicks = () => {
-  clicks.value++;
-};
+  const increaseClicks = () => {
+    clicks.value++;
+  };
 </script>
 
 <template>
-  <div class="flex flex-col flex-1 justify-center items-center gap-5">
+  <div class="flex flex-1 flex-col items-center justify-center gap-5">
     <img src="/vueLogo.png" />
     <p class="text-xl">Vue-Tailwind Template</p>
     <p class="text-xl">{{ t("welcome") }}</p>
     <button
-      class="bg-emerald-500 hover:bg-emerald-600 active:translate-y-[0.1rem] transition text-white font-medium px-5 py-1 rounded"
+      class="rounded bg-emerald-500 px-5 py-1 font-medium text-white transition hover:bg-emerald-600 active:translate-y-[0.1rem]"
       @click="increaseClicks"
     >
       {{ clicks }}
--- src/router.js
+++ src/router.js
@@ -2,9 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const Home = () => import("./pages/Home.vue");
 
-const routes = [
-  { path: "/", component: Home, meta: { title: "Home" } },
-];
+const routes = [{ path: "/", component: Home, meta: { title: "Home" } }];
 
 const router = createRouter({
   history: createWebHistory(),
